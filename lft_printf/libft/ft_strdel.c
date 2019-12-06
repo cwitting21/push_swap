449ef0fc7dcadfb2b12f1f5b579a77e94e63eb67
Carolyne Witting <cwitting@at-i3.21-school.ru>
2019-12-06 07:37:40 +0300

    123

--- lft_printf/libft/ft_strdel.c
+++ lft_printf/libft/ft_strdel.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cwitting <cwitting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/12 17:27:59 by wmaykit           #+#    #+#             */
-/*   Updated: 2019/12/06 06:29:05 by cwitting         ###   ########.fr       */
+/*   Created: 2019/04/12 17:27:59 by cwitting          #+#    #+#             */
+/*   Updated: 2019/12/06 07:30:47 by cwitting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,5 @@
 void	ft_strdel(char **as)
 {
 	if (as && *as)
-	{
-		free(*as);
-		*as = NULL;
-	}
-		// ft_memdel((void**)as);
+		ft_memdel((void**)as);
 }
